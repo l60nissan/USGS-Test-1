dc14b598a35b6ae2caa0f467d15deb45d05fba49
Caitlin Hackett <chackett@usgs.gov>
2021-07-07 12:05:46 -0400

    Initial commit

---
+++ Scripts/LOSOM_workflow.R
@@ -0,0 +1,209 @@
+################################################################
+# Workflow for Netcdf output for restoration runs (LOSOM)
+# Use for Alligator, Everwaders, Snail kite, and Applesnail
+#
+# Caitlin Hackett chackett@usgs.gov
+###############################################################
+
+#Load packages
+library(tidyverse)
+library(ncdf4)
+library(raster)
+library(sf)
+library(cowplot)
+
+source("../Figure Development/Scripts/Final/Second Test/LOSOM_process_functions.R")
+source("../Figure Development/Scripts/Final/Second Test/LOSOM_map_functions.R")
+
+# Species string options
+gator_sp_string <- "Alligator"
+waders_sp_string <- c("GBHE", "GLIB", "GREG", "LBHE", "ROSP", "WHIB", "WOST")
+apsn_sp_string <- "Apple_Snail"
+snki_sp_string <- "SnailKite"
+
+########################################################
+### USER SET FACTORS ###
+
+# Folder containing species output
+PARENT_PATH <- "./Data/LOSOM_Round1_2021_05/Model Output/Alligator/JEM_Alligator_Production_Probability_Model_Data/JEM_Alligator_Production_Probability_Model_Data/"
+
+# Is Spcies output already cropped to AOI (TRUE/FALSE)
+cropped <- FALSE
+
+# Folder to output CSV and figures - include "/" after path to get file names correct when saving 
+OUTPUT_PATH <- "./Output/LOSOM_Round1_2021_05/Alligator/TEST/"
+
+# Path to AOI
+AOI_PATH <- "../../GIS_Library/COP_AOI_mask/COP_AOI_mask.shp"
+#AOI_PATH <- "../../GIS_Library/WERPzones_EDEN_mask.shp/WERPzones_EDEN_mask.shp"
+
+# set target species from species string options at top of script
+sp_string <- gator_sp_string
+
+# Set Alternate scenario names
+#alt_names <- c("Sim_0001")
+#alt_names <- c("ALTHR")
+#alt_names <- c("WECB", "WFWO")
+alt_names <- c("AA", "BB", "CC", "DD", "EE1", "EE2")
+alt_names
+
+# Set Baseline scenario names
+#base_names <- c("Sim_0003", "Sim_0002")
+base_names <- c("ECBr", "NA25")
+#base_names <- c("ALTHR")
+base_names
+#########################################################
+
+# Scenario Name strings
+ALT_NAMES <- paste0(alt_names, collapse = "|")
+ALT_NAMES
+
+# Base Name strings
+BASE_NAMES <- paste0(base_names, collapse = "|")
+BASE_NAMES
+
+# Alt and Base Names
+#if(length(base_names) == 1){BASE_NAMES <- paste0(BASE_NAMES, "|")
+#}else {BASE_NAMES <- BASE_NAMES}
+BASE_NAMES_join <- paste0(BASE_NAMES, "|")
+BASE_NAMES_join
+all_scenario_names <- paste0(BASE_NAMES_join, ALT_NAMES, collapse = "|")
+all_scenario_names
+
+# Loop through spcies, process output, output acreage csv and map
+# Loop thorugh target species
+process_list_all <- list() # list to store all output
+#n <- 1
+for(n in 1:length(sp_string)){ # This part is to accomodate multiple species output in the everwaders path
+
+  FILES <- SP_SCENARIO_FILES(ALT_NAMES, BASE_NAMES, PARENT_PATH, sp_string[n])
+  FILES
+  ALT_LIST <- FILES$ALT_LIST
+  ALT_LIST
+  BASE_LIST <- FILES$BASE_LIST
+  BASE_LIST
+
+  ## Loop to process data and make maps, acreage table and barplot 
+  # Process each baselinee and alt combination
+    process_list <- list() # list to hold processed data
+    percent_diff <- data.frame() # data frame to hold percent difference data for barplot
+    acreage_diff_df <- data.frame() # data frame to hold acreage data 
+    #b <- 1
+    #i <- 1
+    
+    # Process baseline file b
+    for(b in 1:length(BASE_LIST)){  
+      
+      # Create empty list for output for each baseline
+      b_name <- str_extract_all(BASE_LIST[b], all_scenario_names)[[1]]
+      process_list[[b]] <- list()
+      names(process_list)[[b]] <- b_name
+      
+      # process alternate file i
+      for(i in 1:length(ALT_LIST)){
+      
+      a_name <- str_extract_all(ALT_LIST[i], all_scenario_names)[[1]]
+      print(paste0("Processing :: ALT_", a_name, " minus BASE_", b_name))
+  
+      map_dfs <- PROCESS_OUTPUT(BASE_LIST[b], ALT_LIST[i], AOI_PATH, all_scenario_names, CROPPED = cropped)
+  
+      process_list[[b]][[i]] <- map_dfs
+      names(process_list[[b]])[[i]] <- paste0(map_dfs$alt_name, "-", map_dfs$base_name)
+      
+      print(paste0("Making Map :: ALT_", a_name, " minus BASE_", b_name))
+      
+      # Make Map
+      ind_fill <- 'labs'
+      dif_fill <- 'labs'
+      map_title <- map_dfs$map_title
+      out_path <- OUTPUT_PATH
+      out_file <- paste0(out_path, gsub(" ", "_", map_title), "_", map_dfs$alt_name, "_", map_dfs$base_name, ".pdf")
+  
+      print(paste0("Making map for ", gsub(" ", "_", map_title), "_", map_dfs$alt_name, "_", map_dfs$base_name))
+  
+      name.labs <- map_dfs$name.labs
+  
+      RESTORATION_RUN_MAP(DF_IND = map_dfs$ind_df,
+                      IND_FILL = ind_fill,
+                      DF_DIF = map_dfs$diff_df,
+                      DIF_FILL = dif_fill,
+                      SCENARIO_COL = names(map_dfs$ind_df)[5],
+                      YEAR_COL = names(map_dfs$ind_df)[3],
+                      AOI_PATH = AOI_PATH,
+                      MPR_PATH = MPR_PATH,
+                      WCAS_PATH = WCAS_PATH,
+                      FL_PATH = FL_PATH,
+                      MAP_EXTENT = MAP_EXTENT,
+                      MAP_TITLE = map_title,
+                      OUTPUT_FILE_NAME = out_file)
+  
+      # Add data to Acreage dataframe
+      print(paste0("Adding Acreage :: ALT_", a_name, " minus BASE_", b_name))
+      acreage_diff_df <- bind_rows(acreage_diff_df, map_dfs$acreage_df)
+
+      # add data t o percent diff dataframe - if percent diff was calculated during data processing, bind rows to one dataframe
+      #if(grepl(gator_sp_string, FILES$ALL_FILES[1])){
+      print(paste0("Adding Percent Difference :: ALT_", a_name, " minus BASE_", b_name))
+      if("Percent_diff_mean" %in% names(map_dfs)){
+      percent_diff <- bind_rows(percent_diff, map_dfs$Percent_diff_mean)
+      } else {
+      percent_diff <- NULL
+          }
+       }
+    }
+  # Print list with all processed data  
+  process_list_all[[n]] <- process_list
+  names(process_list_all)[[n]] <- sp_string[n]
+  
+  # Export acreage Data
+  print(paste0("Export Acreage Data CSV"))
+  output_filename <- paste0(out_path, "Acreage_", gsub(" ", "_", map_title), ".csv")
+  write.csv(acreage_diff_df, output_filename, row.names = FALSE)
+  
+  # Export dataframe with all percent differences
+  # If percent diffrence data frame exists, create a bar plot
+  print(paste0("Export Percent Difference Data CSV"))
+  if(!is.null(percent_diff)){
+  diff_output_filename <- paste0(out_path, "PercentDiff_", gsub(" ", "_", map_title), ".csv")
+  write.csv(percent_diff, diff_output_filename, row.names = FALSE)
+  
+  # Make Percent diffrence bar plot
+  x_var <- "Year"
+  y_var <- "Percent_Difference"
+  fill_var <- "Scenarios"
+  title <- map_dfs$map_title
+  #y_lab <- paste0("Percent Change in ", title, "\nfrom baseline to ", map_dfs$alt_name)
+  x_lab <- "Year"
+  min_limit <-plyr::round_any(min(percent_diff[y_var]), 5, f = floor) - 10
+  max_limit <- plyr::round_any(max(percent_diff[y_var]), 5, f = ceiling) + 10
+  
+
+  for(a in 1:length(alt_names)){
+    print(paste0("Making Differnce Bar Plot :: ", alt_names[a]))
+    
+    per_diff_alt <- percent_diff[grep(alt_names[a], percent_diff$Scenarios),]
+    TEST <- PER_DIFF_PLOT(
+      DF = per_diff_alt,
+      X_VAR = x_var,
+      Y_VAR = y_var,
+      FILL_VAR = fill_var,
+      TITLE = title,
+      Y_LAB = paste0("Percent Change in ", title, "\nfrom Baseline to ", alt_names[a]),
+      X_LAB = x_lab,
+      MIN_LIMIT = min_limit,
+      MAX_LIMIT = max_limit
+    )
+    diff_plot_filename <- paste0(out_path, "PercentDiff_BarPlot_", gsub(" ", "_", map_title),"_", alt_names[a], ".pdf")
+    ggsave(diff_plot_filename, TEST, width=11, height=8.5, units="in", dpi=300)
+  }}
+  
+}
+
+# Save R object that has processed data stored
+  if(grepl("EverWaders", FILES$ALL_FILES[1])){
+  save_string <- "EverWaders"
+  }else{
+  save_string <- sp_string}
+
+save(process_list_all, FILES, file = paste0(out_path, save_string, "_processed_data.RData"))
+load('./Output/LOSOM_Round1_2021_05/Alligator/Alligator_Round1/Alligator_processed_data.RData')
---
+++ Scripts/MARL_workflow.R
@@ -0,0 +1,129 @@
+######################################
+# Workflow for Restoration Run MArl Praire Output
+#
+# Caitlin Hackett chackett@usgs.gov
+######################################
+
+source("../Figure Development/Scripts/Final/Second Test/MARL_process_function.R")
+source("../Figure Development/Scripts/Final/Second Test/MARL_map_functions.R")
+
+
+library(tidyverse)
+library(sf)
+library(raster)
+
+########################################################
+### USER SET FACTORS ###
+
+# Folder containing species output
+PARENT_PATH <- "./Data/LOSOM_Round1_2021_05/Model Output/MarlPrairie/MarlPrairie_Data/MarlPrairie_Data/"
+
+# Folder to output CSV and figures - include "/" after path to get file names correct when saving 
+OUTPUT_PATH <- "./Output/LOSOM_Round1_2021_05/MarlPrairie/test/"
+
+# Marl Shapefile Path
+mp_shp <- "./Data/LOSOM_Round1_2021_05/Model Output/MarlPrairie/MarlPrairie_Data/MarlPrairie_Data/depth_AA/MP_Scores.shp"
+
+# Set Alternate scenario names
+#alt_names <- c("Sim_0001")
+alt_names <- c("AA", "BB", "CC", "DD", "EE1", "EE2")
+#alt_names <- c("WECB", "WFWO")
+alt_names
+
+# Set Baseline scenario names
+#base_names <- c("Sim_0003", "Sim_0002")
+base_names <- c("ECBr", "NA25")
+#base_names <- c("ALTHR")
+base_names
+#########################################################
+
+# Scenario Name strings
+ALT_NAMES <- paste0(alt_names, collapse = "|")
+ALT_NAMES
+
+# Base Name strings
+BASE_NAMES <- paste0(base_names, collapse = "|")
+BASE_NAMES
+
+# Alt and Base Names
+#if(length(BASE_NAMES) == 1){BASE_NAMES <- paste0(BASE_NAMES, "|")}
+BASE_NAMES_join <- paste0(BASE_NAMES, "|")
+all_scenario_names <- paste0(BASE_NAMES_join, ALT_NAMES, collapse = "|")
+all_scenario_names
+
+# All Files
+ALL_FILES <- list.files(PARENT_PATH, pattern = "\\MP_Scores.csv", full.names = TRUE, recursive = TRUE)
+ALL_FILES
+
+# Alt Files
+ALT_LIST <- grep(ALT_NAMES, value = T, ALL_FILES)
+ALT_LIST
+
+# Base Files
+BASE_LIST <- grep(BASE_NAMES, value = T, ALL_FILES)
+BASE_LIST
+
+
+## Process data for all baseline and alternate combinations
+marl_process_list <- list()
+acreage_diff_df <- data.frame()
+  for(b in 1:length(BASE_LIST)){
+    
+    b_name <- str_extract_all(BASE_LIST[b], all_scenario_names)[[1]]
+    marl_process_list[[b]] <- list()
+    names(marl_process_list)[[b]] <- b_name
+    
+    for(a in 1:length(ALT_LIST)){
+      a_name <- str_extract_all(ALT_LIST[a], all_scenario_names)[[1]]
+      print(paste0("Processing :: ALT_", a_name, " minus BASE_", b_name))
+      marl_process <- MARL_PROCESS_OUTPUT(BASE_LIST[b], ALT_LIST[a], mp_shp)
+    
+      marl_process_list[[b]][[a]]<- marl_process
+      names(marl_process_list[[b]])[[a]] <- marl_process$diff_name
+    
+      # Add acreage df to large daeframe with all marl acreage
+      print(paste0("Adding Acreage data :: ALT_", a_name, " minus BASE_", b_name))
+      acreage_diff_df <- bind_rows(acreage_diff_df, marl_process$acreage_df)
+    
+      # Make Map
+      print(paste0("Making Map :: ALT_", a_name, " minus BASE_", b_name))
+      
+      
+      ind_fill <- 'labs'
+      dif_fill <- 'labs'
+      map_title <- marl_process$Map_title
+      out_path <- OUTPUT_PATH
+      out_file <- paste0(out_path,"Marl_HSI_", marl_process$diff_name, ".tiff")
+    
+      MARL_MAP(DF_IND = marl_process$ind_df,
+             IND_FILL = ind_fill,
+             DF_DIF = marl_process$diff_df,
+             DIF_FILL = dif_fill,
+             SCENARIO_COL = names(marl_process$ind_df),
+             SPOP_PATH = SUBPOP_PATH,
+             MPR_PATH = MPR_PATH,
+             WCAS_PATH = WCAS_PATH,
+             MAP_EXTENT = MAP_EXTENT,
+             MAP_TITLE = marl_process$Map_title,
+             OUTPUT_FILE_NAME = out_file)
+      }
+  }
+
+# Save acreage Table
+write.csv(acreage_diff_df, file = paste0(OUTPUT_PATH,"Acreage_", marl_process$sp_string, ".csv"), row.names = FALSE)
+
+# Save Marl RDATA of processed files
+save(marl_process_list, ALL_FILES, file = paste0(OUTPUT_PATH, marl_process$sp_string, "_processed_data.RData"))
+
+
+######
+# Examine output since difference maps look similar, values are different but falling within the same bin      
+ecbr_diff <- lapply(marl_process_list$ECBr, function(x) x$diff_df)
+ecbr_diff_values <- lapply(ecbr_diff, function(x) x$hsi_CombinedScore)
+ecbr_diff_df <- bind_cols(ecbr_diff_values)
+ecbr_diff_df <- bind_cols(ecbr_diff_df, marl_process_list$ECBr$`AA-ECBr`$diff_df$RSM)
+
+na25_diff <- lapply(marl_process_list$NA25, function(x) x$diff_df)
+na25_diff_values <- lapply(na25_diff, function(x) x$hsi_CombinedScore)
+na25_diff_df <- bind_cols(na25_diff_values)
+na25_diff_df <- bind_cols(na25_diff_df, marl_process_list$ECBr$`AA-ECBr`$diff_df$RSM)
