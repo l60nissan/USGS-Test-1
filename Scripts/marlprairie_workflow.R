60edbdbf70e35328a68bf1e497c55ca9b1ed397e
Caitlin Hackett <chackett@usgs.gov>
2023-04-26 15:19:18 -0400

    update to source inputs and update style to match style guide

--- Scripts/marlprairie_workflow.R
+++ Scripts/marlprairie_workflow.R
@@ -4,81 +4,93 @@
 # Caitlin Hackett chackett@usgs.gov
 ######################################
 
-source("../Figure Development/Scripts/Final/Second Test/MARL_process_function.R")
-source("../Figure Development/Scripts/Final/Second Test/MARL_map_functions.R")
-
-
+# Load packages
 library(tidyverse)
 library(sf)
 library(raster)
+
+# Source dependancy scripts
+# Functions to process data nd make bar plots
+source("./Scripts/marlprairie_process_function.R")
+
+# Function to generate maps
+source("./Scripts/marlprairie_map_function.R")
+
+# Defined file paths for shapefiles
+source("./Scripts/input_paths.R")
+
+# Defined species strings - for consistency across coordinated scripts
+source("./Scripts/species_string_definitions.R")
+
 
 ########################################################
 ### USER SET FACTORS ###
 
 # Folder containing species output
-PARENT_PATH <- "./Data/LOSOM_Round1_2021_05/Model Output/MarlPrairie/MarlPrairie_Data/MarlPrairie_Data/"
+#PARENT_PATH <- "./Data/LOSOM_Round1_2021_05/Model Output/MarlPrairie/MarlPrairie_Data/MarlPrairie_Data/"
 
 # Folder to output CSV and figures - include "/" after path to get file names correct when saving 
-OUTPUT_PATH <- "./Output/LOSOM_Round1_2021_05/MarlPrairie/test/"
+#OUTPUT_PATH <- "./Output/LOSOM_Round1_2021_05/MarlPrairie/test/"
 
 # Marl Shapefile Path
-mp_shp <- "./Data/LOSOM_Round1_2021_05/Model Output/MarlPrairie/MarlPrairie_Data/MarlPrairie_Data/depth_AA/MP_Scores.shp"
+#mp_shp <- "./Data/LOSOM_Round1_2021_05/Model Output/MarlPrairie/MarlPrairie_Data/MarlPrairie_Data/depth_AA/MP_Scores.shp"
 
 # Set Alternate scenario names
 #alt_names <- c("Sim_0001")
-alt_names <- c("AA", "BB", "CC", "DD", "EE1", "EE2")
+#alt_names <- c("AA", "BB", "CC", "DD", "EE1", "EE2")
 #alt_names <- c("WECB", "WFWO")
-alt_names
+#alt_names
 
 # Set Baseline scenario names
 #base_names <- c("Sim_0003", "Sim_0002")
-base_names <- c("ECBr", "NA25")
+#base_names <- c("ECBr", "NA25")
 #base_names <- c("ALTHR")
-base_names
+#base_names
 #########################################################
 
 # Scenario Name strings
-ALT_NAMES <- paste0(alt_names, collapse = "|")
-ALT_NAMES
+alt_names <- paste0(alt_names, collapse = "|")
+alt_names
 
 # Base Name strings
-BASE_NAMES <- paste0(base_names, collapse = "|")
-BASE_NAMES
+base_names <- paste0(base_names, collapse = "|")
+base_names
 
 # Alt and Base Names
-#if(length(BASE_NAMES) == 1){BASE_NAMES <- paste0(BASE_NAMES, "|")}
-BASE_NAMES_join <- paste0(BASE_NAMES, "|")
-all_scenario_names <- paste0(BASE_NAMES_join, ALT_NAMES, collapse = "|")
+#if(length(base_names) == 1){base_names <- paste0(base_names, "|")}
+base_names_join <- paste0(base_names, "|")
+all_scenario_names <- paste0(base_names_join, alt_names, collapse = "|")
 all_scenario_names
 
 # All Files
-ALL_FILES <- list.files(PARENT_PATH, pattern = "\\MP_Scores.csv", full.names = TRUE, recursive = TRUE)
-ALL_FILES
+all_files <- list.files(paren_path, pattern = "\\MP_Scores.csv",
+                        full.names = TRUE, recursive = TRUE)
+all_files
 
 # Alt Files
-ALT_LIST <- grep(ALT_NAMES, value = TRUE, ALL_FILES)
-ALT_LIST
+alt_list <- grep(alt_names, value = TRUE, all_files)
+alt_list
 
 # Base Files
-BASE_LIST <- grep(BASE_NAMES, value = TRUE, ALL_FILES)
-BASE_LIST
+base_list <- grep(base_names, value = TRUE, all_files)
+base_list
 
 
 ## Process data for all baseline and alternate combinations
 marl_process_list <- list()
 acreage_diff_df <- data.frame()
-  for(b in 1:length(BASE_LIST)){
+  for (b in 1:length(base_list)) {
     
-    b_name <- str_extract_all(BASE_LIST[b], all_scenario_names)[[1]]
+    b_name <- str_extract_all(base_list[b], all_scenario_names)[[1]]
     marl_process_list[[b]] <- list()
     names(marl_process_list)[[b]] <- b_name
     
-    for(a in 1:length(ALT_LIST)){
-      a_name <- str_extract_all(ALT_LIST[a], all_scenario_names)[[1]]
+    for (a in 1:length(alt_list)) {
+      a_name <- str_extract_all(alt_list[a], all_scenario_names)[[1]]
       print(paste0("Processing :: ALT_", a_name, " minus BASE_", b_name))
-      marl_process <- MARL_PROCESS_OUTPUT(BASE_LIST[b], ALT_LIST[a], mp_shp)
+      marl_process <- MARL_PROCESS_OUTPUT(base_list[b], alt_list[a], mp_shp)
     
-      marl_process_list[[b]][[a]]<- marl_process
+      marl_process_list[[b]][[a]] <- marl_process
       names(marl_process_list[[b]])[[a]] <- marl_process$diff_name
     
       # Add acreage df to large daeframe with all marl acreage
@@ -110,10 +122,14 @@ acreage_diff_df <- data.frame()
   }
 
 # Save acreage Table
-write.csv(acreage_diff_df, file = paste0(OUTPUT_PATH,"Acreage_", marl_process$sp_string, ".csv"), row.names = FALSE)
+write.csv(acreage_diff_df,
+          file = paste0(OUTPUT_PATH,"Acreage_", marl_process$sp_string, ".csv"),
+          row.names = FALSE)
 
 # Save Marl RDATA of processed files
-save(marl_process_list, ALL_FILES, file = paste0(OUTPUT_PATH, marl_process$sp_string, "_processed_data.RData"))
+save(marl_process_list, all_files,
+     file = paste0(OUTPUT_PATH, marl_process$sp_string,
+                   "_processed_data.RData"))
 
 
 ######
