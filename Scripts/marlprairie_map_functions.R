5d93173abf36f405c8a713de971b545738950302
Caitlin Hackett <chackett@usgs.gov>
2023-04-27 17:34:33 -0400

    update comments

--- Scripts/marlprairie_map_functions.R
+++ Scripts/marlprairie_map_functions.R
@@ -20,13 +20,13 @@ library(shadowtext)
 
 MarlMap <- function(
     df_ind,       # Dataframe of individual score output with coordinate column names "x", and "y", 
-    ind_fill,     # column name of individual score discrete values to be plotted (LOSOM is "labs")
+    ind_fill,     # column name of individual score discrete values to be plotted (ex. "labs")
     df_dif,       # Dataframe of difference output with coordinate column names "x", and "y"
-    dif_fill,     # column name of discrete values to be plotted (LOSOM is "labs")
+    dif_fill,     # column name of discrete values to be plotted (ex. "labs")
     scenario_col, # column name of scenarios to be plotted
     mpr_path,     # File path to shapefile of Main Park Road
     wcas_path,    # File path to shapefile of water conservation areas boundaries
-    spop_path,    # File path to shapefile of subpopulation boundaries
+    spop_path,    # File path to shapefile of CSSS subpopulation boundaries
     map_extent,   # Extent of map, format: c(xmin, xmax, ymin, ymax)
     map_title,    # Title to be printed at top of map, (ex. "Alligator HSI")
     output_file_name){# Name of Output File
@@ -76,7 +76,6 @@ MarlMap <- function(
     mpr_shp <- st_make_valid(mpr_shp)}  
   
   # Load CSSS Subpopulations
-  # Load AOI mask shapefile
   spop_shp <- st_read(dsn = spop_path) %>%
     st_transform(crs = 26917)
   spop_st_valid <- st_is_valid(spop_shp)
@@ -133,6 +132,8 @@ MarlMap <- function(
             linewidth = scale_factor*0.5, show.legend = FALSE) +
     geom_sf(data = spop.crop, colour = "Red", alpha = 0,
             linewidth = scale_factor*0.4, show.legend = FALSE) +
+    
+    # Add labels for Cape Sable seaside Sparrow Subpopulations
     geom_shadowtext(data = filter(subpop_coord,
                                   SubPopulat != "A" & SubPopulat != "AX"),
                     aes(x = X, y = Y,label = SubPopulat),
@@ -172,103 +173,123 @@ MarlMap <- function(
     # format x axis text
     axis.text.x = element_text(size = 13), 
     
+    #set size/color of text in strip headings on the facets
     strip.background = element_rect(colour = "Black"),
-    
-    #set size of text in strip headings on the facets
     strip.text = element_text(size = legend_scale_factor*13), 
-    axis.title = element_blank(), # Remove axis titles
+    
+    # Remove axis titles
+    axis.title = element_blank(),
+    
+    # Set plot margins
     plot.margin = margin(1.5,1,0,1, unit = "cm"),
+    
+    # Format legend format and text
     legend.key.height = unit(5, "mm"),
     legend.key.width = unit(5, "mm"),
     legend.key = element_rect(colour = "black", size = 0.5),
     legend.title.align = 0.0,
     legend.margin = margin(0,0,0,0, unit = "mm"),
     text = element_text(family = "sans", size = 16)) 
-ind_plot
+  
 
-diff_plot <- ggplot() +
-  geom_sf(data = df_dif, aes(fill = !!sym(dif_fill)),
-          color = "gray25", linewidth = 0.05) +
-  scale_fill_manual(values = diff_pal, name = diff_legend_name, drop = FALSE) +
-  guides(fill = guide_legend(ncol = 1, reverse = TRUE,
-                             override.aes = list(color = NA))) +
+  #-----------------
+  # Create Difference Map Plot
   
-  facet_wrap(~Scenario) +
+  diff_plot <- ggplot() +
+    
+    # Plot difference between individual scores 
+    geom_sf(data = df_dif, aes(fill = !!sym(dif_fill)),
+            color = "gray25", linewidth = 0.05) +
+    scale_fill_manual(values = diff_pal, name = diff_legend_name, drop = FALSE) +
+    guides(fill = guide_legend(ncol = 1, reverse = TRUE,
+                               override.aes = list(color = NA))) +
+    
+    # Plot Facets - this will creat the correct facet labels since only one map
+    # in panel for the difference map
+    facet_wrap(~Scenario) +
+    
+    # Plot shapefiles for Main Park Road, WCAS, and area of interest
+    geom_sf(data = mpr.crop, colour = "black",
+            linewidth = scale_factor*0.5, show.legend = F, lty = "dashed") +
+    geom_sf(data = wca.crop, colour = "black", alpha = 0, 
+            linewidth = scale_factor*0.5, show.legend = FALSE) +
+    geom_sf(data = spop.crop, colour = "Red", alpha = 0,
+            linewidth = scale_factor*0.4, show.legend = FALSE) +
+    geom_sf_text(data = filter(spop.crop, SubPopulat != "A" & SubPopulat != "AX"),
+                 aes(label = SubPopulat),
+                 color = "black", size = 5, fontface = "bold") +
+    
+    # Add labels for Cape Sable seaside Sparrow Subpopulations
+    annotate("text", x = 509491.978, y = 2837582.318,
+             label = "A", size = 5, fontface = "bold") +
+    annotate("text", x = 496950.703, y = 2835201.064,
+             label = "A", size = 5, fontface = "bold") +
+    annotate("text", x = 518937.622, y = 2844011.706,
+             label = "AX", size = 5, fontface = "bold") +
+    annotate("text", x = 514730.738, y = 2827819.174,
+             label = "AX", size = 5, fontface = "bold") +
   
-  # Plot shapefiles for Main Park Road, WCAS, and area of interest
-  geom_sf(data = mpr.crop, colour = "black",
-          linewidth = scale_factor*0.5, show.legend = F, lty = "dashed") +
-  geom_sf(data = wca.crop, colour = "black", alpha = 0, 
-          linewidth = scale_factor*0.5, show.legend = FALSE) +
-  geom_sf(data = spop.crop, colour = "Red", alpha = 0,
-          linewidth = scale_factor*0.4, show.legend = FALSE) +
-  geom_sf_text(data = filter(spop.crop, SubPopulat != "A" & SubPopulat != "AX"),
-               aes(label = SubPopulat),
-               color = "black", size = 5, fontface = "bold") +
-  annotate("text", x = 509491.978, y = 2837582.318,
-           label = "A", size = 5, fontface = "bold") +
-  annotate("text", x = 496950.703, y = 2835201.064,
-           label = "A", size = 5, fontface = "bold") +
-  annotate("text", x = 518937.622, y = 2844011.706,
-           label = "AX", size = 5, fontface = "bold") +
-  annotate("text", x = 514730.738, y = 2827819.174,
-           label = "AX", size = 5, fontface = "bold") +
+    # Add scalebar
+    ggsn::scalebar(data = df_scale, transform = F, dist = 10, dist_unit = "km",
+                   st.dist = 0.009, st.size = 3.5, st.bottom = TRUE,
+                   height = 0.008, border.size = 1,
+                   anchor = c(x = 508000, y = 2794151), family = "sans") +
+    
+    # Add north arrow
+    ggsn::north(data = df_scale, symbol = 12, scale = 0.03,
+                x.min = NULL, x.max = NULL, y.min = NULL, y.max = NULL,
+                anchor = c(x = 493000, y = 2800500)) +
+    
+        # Can use this to set crs or crop the map output if needed,
+    # Expand = FALSE does not add extra buffer around plotted extent
+    coord_sf(expand = F, xlim = c(map_extent[1], map_extent[2]),
+             ylim = c(map_extent[3], map_extent[4])) +
+    
+    # Set which graticule displayed on x and y axis
+    scale_x_continuous(breaks = c(-80.5, -81, -81.5)) +
+    scale_y_continuous(breaks = c(25.5, 26.0)) +
+    
+    # Set theme elements
+    theme(
+      panel.grid.major = element_blank(), # remove grid lines
+      panel.grid.minor = element_blank(), # remove grid lines,
+      
+      panel.border = element_rect(fill = NA, color = "black", linewidth = 1),
+      
+      # rotate and position y axis labels
+      axis.text.y = element_text(angle = 90, hjust = 0.5, vjust = 0.0, size = 13), 
+      
+      # format x axis text
+      axis.text.x = element_text(size = 13), 
+      
+      #set size/color of text in strip headings on the facets
+      strip.background = element_rect(colour = "Black"),
+      strip.text = element_text(size = legend_scale_factor*13), 
+      axis.title = element_blank(), # Remove axis titles
+      
+      # Set plot margins
+      plot.margin = margin(0.2,1,1,1, unit = "cm"),
+      
+      # Format legend format and text
+      legend.key.height = unit(5, "mm"),
+      legend.key.width = unit(5, "mm"),
+      legend.key = element_rect(colour = "black", size = 0.8),
+      legend.title.align = 0.0,
+      legend.margin = margin(0,0,0,0, unit = "mm"),
+      text = element_text(family = "sans", size = 16)) 
 
-  # Add scalebar
-  ggsn::scalebar(data = df_scale, transform = F, dist = 10, dist_unit = "km",
-                 st.dist = 0.009, st.size = 3.5, st.bottom = TRUE,
-                 height = 0.008, border.size = 1,
-                 anchor = c(x = 508000, y = 2794151), family = "sans") +
-  ggsn::north(data = df_scale, symbol = 12, scale = 0.03,
-              x.min = NULL, x.max = NULL, y.min = NULL, y.max = NULL,
-              anchor = c(x = 493000, y = 2800500)) +
-  
-  
-  # Can use this to set crs or crop the map output if needed,
-  # Expand = FALSE does not add extra buffer around plotted extent
-  coord_sf(expand = F, xlim = c(map_extent[1], map_extent[2]),
-           ylim = c(map_extent[3], map_extent[4])) +
-  
-  # Set which graticule displayed on x and y axis
-  scale_x_continuous(breaks = c(-80.5, -81, -81.5)) +
-  scale_y_continuous(breaks = c(25.5, 26.0)) +
-  
-  # Set theme elements
-  theme(
-    panel.grid.major = element_blank(), # remove grid lines
-    panel.grid.minor = element_blank(), # remove grid lines,
-    
-    #panel.background = element_rect(fill = "white"),
-    panel.border = element_rect(fill = NA, color = "black", linewidth = 1),
-    
-    # rotate and position y axis labels
-    axis.text.y = element_text(angle = 90, hjust = 0.5, vjust = 0.0, size = 13), 
-    # format x axis text
-    axis.text.x = element_text(size = 13), 
-    
-    strip.background = element_rect(colour = "Black"),
-    
-    #set size of text in strip headings on the facets
-    strip.text = element_text(size = legend_scale_factor*13), 
-    axis.title = element_blank(), # Remove axis titles
-    plot.margin = margin(0.2,1,1,1, unit = "cm"),
-    
-    legend.key.height = unit(5, "mm"),
-    legend.key.width = unit(5, "mm"),
-    legend.key = element_rect(colour = "black", size = 0.8),
-    legend.title.align = 0.0,
-    legend.margin = margin(0,0,0,0, unit = "mm"),
-    text = element_text(family = "sans", size = 16)) 
-diff_plot
-
-comb <- plot_grid(ind_plot, diff_plot,
+  # Combine individual maps and difference map
+  comb <- plot_grid(ind_plot, diff_plot,
                   ncol = 1, nrow = 2,
                   rel_widths = c(1, 1), rel_heights = c(1, 1))
-comb <- ggdraw(comb) +
-  draw_label(map_title, x = 0.32, y = .95, vjust = 0,
+  
+  # Add map title
+  comb <- ggdraw(comb) +
+          draw_label(map_title, x = 0.32, y = .95, vjust = 0,
              fontfamily = "serif", size = 20)
-
-ggsave(output_file_name, comb,
+  
+  # Save final map
+  ggsave(output_file_name, comb,
        height = 8.5, width = 11, units = "in", scale = 1, bg = "white")
 }
 
