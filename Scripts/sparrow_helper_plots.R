8e5bd90b38aebf4192a1d17c112f1ad6db96f989
Caitlin Hackett <chackett@usgs.gov>
2023-05-09 15:03:15 -0400

    clean up to remove scenario names and file paths since sourced from other files during "run" process

--- Scripts/sparrow_helper_plots.R
+++ Scripts/sparrow_helper_plots.R
@@ -1,33 +1,11 @@
-## ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Generate Sparrow Helper Plots
 # Create Annual summary of 4-yr hydroperiod data: sparrow_tsp.xlsx
-## ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 # Load packages
 library(tidyverse)
 library(writexl)
-
-#alt_names <- c("AA", "BB", "CC", "DD", "EE1", "EE2")
-#alt_names
-
-# Set Baseline scenario names
-#base_names <- c("ECBr", "NA25")
-#base_names <- c("ALTHR")
-#base_names
-
-# Scenario Name strings
-#ALT_NAMES <- paste0(alt_names, collapse = "|")
-#ALT_NAMES
-
-# Base Name strings
-#BASE_NAMES <- paste0(base_names, collapse = "|")
-#BASE_NAMES
-
-# Alt and Base Names
-#if(length(BASE_NAMES) == 1){BASE_NAMES <- paste0(BASE_NAMES, "|")}
-#BASE_NAMES_join <- paste0(BASE_NAMES, "|")
-#all_scenario_names <- paste0(BASE_NAMES_join, ALT_NAMES, collapse = "|")
-#all_scenario_names
 
 scenario_names <- c(alt_names, base_names)
 scenario_names
@@ -35,11 +13,6 @@ scenario_names
 # Make vector with "|" between each scenario 
 all_scenario_names <- paste0(scenario_names, collapse = "|")
 all_scenario_names
-
-
-#PARENT_PATH <- "../LOSOM/Data/LOSOM_Round1_2021_05/Model Output/CSSSHelper/JEM_Sparrow_Helper_Data/JEM_Sparrow_Helper_Data/"
-
-#OUTPUT_PATH <- "../LOSOM/Output/LOSOM_Round1_2021_05/CSSS/"
 
 # List Files
 all_files <- list.files(path = parent_path,
@@ -63,12 +36,15 @@ hydro_percent <- pivot_longer(hydro_percent, cols = c(2:7))
 
 
 ##rename and relevel popa scenarios
-hydro_percent$Scenario <- factor(hydro_percent$Scenario, levels = c(alt_names, base_names))
-hydro_percent$name <- factor(hydro_percent$name, levels = c("A", "B", "C", "D", "E", "F"))
+hydro_percent$Scenario <- factor(hydro_percent$Scenario,
+                                 levels = c(alt_names, base_names))
+hydro_percent$name <- factor(hydro_percent$name,
+                             levels = c("A", "B", "C", "D", "E", "F"))
 pops <- levels(hydro_percent$name)
 
 # set palette for bar plots
-bar_pal <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#000000")
+bar_pal <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442",
+             "#0072B2", "#D55E00", "#CC79A7", "#000000")
 
 for (p in 1:length(pops)) {
   plot_data <- hydro_percent[hydro_percent$name == pops[p],]
@@ -77,7 +53,8 @@ for (p in 1:length(pops)) {
 
 csss_plot <- ggplot(data = plot_data,
                     aes(x = as.factor(Date), y = value, fill = Scenario)) +
-  geom_bar(stat = "identity", position = "dodge", width = 0.7, colour = "black") +
+  geom_bar(stat = "identity", position = "dodge",
+           width = 0.7, colour = "black") +
   scale_fill_manual(values = bar_pal ) +
   ylab("Proportion of Area Meeting Conditions") + xlab("Year") +
   ggtitle(paste0("Cape Sable Seaside Sparrow - Subpopulation ", pops[p])) +
