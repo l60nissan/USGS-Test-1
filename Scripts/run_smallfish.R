16a30f81dd82fabcf3436f8cc67b0d7489beb6dd
Caitlin Hackett <chackett@usgs.gov>
2023-05-09 11:15:03 -0400

    Initial commit

---
+++ Scripts/run_smallfish.R
@@ -0,0 +1,16 @@
+## -----------------------------------------------------------------------------
+# Run Alligator workflow
+## -----------------------------------------------------------------------------
+print(paste0("SMALL FISH :: START -- ", Sys.time()))
+
+# Source inputs/definitions
+source("./Scripts/workflow_inputs.R") # workflow inputs
+
+# Set inputs for workflow
+parent_path <- fish_path # parent path
+output_path <- fish_output_path # output path
+
+# source/run workflow
+source("./Scripts/smallfish_workflow.R")
+
+print(paste0("SMALL FISH :: END -- ", Sys.time()))
