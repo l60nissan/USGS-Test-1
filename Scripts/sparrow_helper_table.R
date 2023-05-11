dcae1ab3e36d49e7afc942bbc0ed6dd46fe2d378
Caitlin Hackett <chackett@usgs.gov>
2023-05-11 11:14:05 -0400

    add and edit comments

--- Scripts/sparrow_helper_table.R
+++ Scripts/sparrow_helper_table.R
@@ -8,10 +8,10 @@ library(tidyverse)
 ## ------------------------------------------------------------------------------
 # Calculations for table
 # 
-# "Table: Difference in the number of days for which the daily mean depths
+# Table: Difference in the number of days for which the daily mean depths
 # are within the target depth range (-50.0 to -25.0 cm) from 1965 through 2005,
 # where the difference is the number of days in the tentatively selected plan
-# minus the number of days in the baseline."
+# minus the number of days in the baseline
 ## ------------------------------------------------------------------------------
 
 # Vector of all scenario names - scenario names set in workflow_inputs.R
@@ -22,6 +22,7 @@ scenario_names
 all_scenario_names <- paste0(scenario_names, collapse = "|")
 all_scenario_names
 
+# List all csv files that contain the target pattern
 all_files <- list.files(path = parent_path,
                         pattern = "depth_average_\\(-50.0 to -25.0).csv",
                         recursive = TRUE, full.names = TRUE)
@@ -30,16 +31,22 @@ all_files
 #Load all files and add scenario name
 all_runs <- data.frame()
 for (s in 1:length(all_files)) {
-  name <- str_extract_all(all_files[s], all_scenario_names)[[1]]
   
+  # extract alternative or baseline scenario from each file
+  name <- str_extract_all(all_files[s], all_scenario_names)[[1]]
+
+  # Read the csv file
   file <- read.csv(all_files[s])
   
+  # Create column that contatins scenario name
   file$Scenario <- name
-
+  
+  # Bind rows read csv file to previously read data to create one dataframe 
+  # that conatains data from all files
   all_runs <- bind_rows(all_runs, file)  
   }
 
-# Make long - letters represent subpopulations
+# Make long: letters represent subpopulations
 runs.long <- pivot_longer(all_runs, c("A", "B","C","D", "E","F"))
 
 # Group and sum each subpopulation for each group
@@ -50,23 +57,32 @@ runs.summary <- runs.long %>%
 #Calculate difference
 num_day_diff <- data.frame()
 for (b in 1:length(base_names)) {
+  
+  # Pull baseline name
   b_name <- base_names[b]
   
   for (a in 1:length(alt_names)) {
-
+    
+    # Pull alternative line
     a_name <- alt_names[a]
     
+    # Extract summary for target alternative (a) and baseline (b)
     ab_df <- runs.summary[runs.summary$Scenario == a_name | runs.summary$Scenario == b_name,]
     
+    # Pivot data to wide format
     ab_df <- pivot_wider(ab_df, id_cols = "name",
                          names_from = "Scenario", values_from = "num.days")
     
+    # make scenario name alternative-baseline
     ab_df$Scenario <- paste0(a_name, "-", b_name)
     
+    # Calculate differnce between alternative and baseline
     ab_df$Difference <- ab_df[[a_name]] - ab_df[[b_name]]
     
+    # Select columns of interest
     diff_days <- dplyr::select(ab_df, c(name, Scenario, Difference))
 
+    # Bind difference calculations together to one dataframe 
     num_day_diff <- bind_rows(num_day_diff, diff_days)
   }
 }
