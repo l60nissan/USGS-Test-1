a98c023663e888569d77b2d065cd4752c9a9cffe
Caitlin Hackett <chackett@usgs.gov>
2023-05-12 14:29:42 -0400

    add print INFO statements

--- Scripts/run_everwaders.R
+++ Scripts/run_everwaders.R
@@ -1,10 +1,16 @@
 ## -----------------------------------------------------------------------------
 # Run EverWaders workflow
 ## -----------------------------------------------------------------------------
-print(paste0("EVERWADERS :: START -- ", Sys.time()))
+print(paste0("INFO [", Sys.time(), "] EverWaders :: START"))
+
+# Install any necessary packages that are not yet installed
+source("./Scripts/workflow_packages.R")
 
 # Source inputs/definitions
+print(paste0("INFO [", Sys.time(), "] Loading Workflow inputs"))
 source("./Scripts/workflow_inputs.R") # workflow inputs
+
+print(paste0("INFO [", Sys.time(), "] Loading Species String Definitions"))
 source("./Scripts/species_string_definitions.R") # species strings
 
 # Set inputs for workflow
@@ -16,4 +22,4 @@ cropped <- waders_cropped # cropped?
 # source/run workflow
 source("./Scripts/rest_run_workflow.R")
 
-print(paste0("EVERWADERS :: END -- ", Sys.time()))
+print(paste0("INFO [", Sys.time(), "] EverWaders :: END"))
