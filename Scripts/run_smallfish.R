96143f6de94b9baf89463ff24fc492aa1e6cddff
Caitlin Hackett <chackett@usgs.gov>
2023-05-14 15:57:51 -0400

    add print statements

--- Scripts/run_smallfish.R
+++ Scripts/run_smallfish.R
@@ -1,9 +1,10 @@
 ## -----------------------------------------------------------------------------
 # Run Small Fish workflow
 ## -----------------------------------------------------------------------------
-print(paste0("SMALL FISH :: START -- ", Sys.time()))
+print(paste0("INFO [", Sys.time(), "] Small Fish :: START"))
 
 # Source inputs/definitions
+print(paste0("INFO [", Sys.time(), "] Loading Workflow inputs"))
 source("./Scripts/workflow_inputs.R") # workflow inputs
 
 # Set inputs for workflow
@@ -13,4 +14,4 @@ output_path <- fish_output_path # output path
 # source/run workflow
 source("./Scripts/smallfish_workflow.R")
 
-print(paste0("SMALL FISH :: END -- ", Sys.time()))
+print(paste0("INFO [", Sys.time(), "] Small Fish :: END"))
