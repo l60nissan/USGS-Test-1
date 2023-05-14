24fb8118ba8dd7f069e17429ad15c64530961243
Caitlin Hackett <chackett@usgs.gov>
2023-05-14 16:01:59 -0400

    add print/INFO statements

--- Scripts/run_applesnail.R
+++ Scripts/run_applesnail.R
@@ -1,10 +1,16 @@
 ## -----------------------------------------------------------------------------
 # Run Apple Snail workflow
 ## -----------------------------------------------------------------------------
-print(paste0("APPLE SNAIL :: START -- ", Sys.time()))
+print(paste0("INFO [", Sys.time(), "] Apple Snail :: START"))
+
+# Install any necessary packages that are not yet installed
+source("./Scripts/workflow_packages.R")
 
 # Source inputs/definitions
+print(paste0("INFO [", Sys.time(), "] Loading Workflow inputs"))
 source("./Scripts/workflow_inputs.R") # workflow inputs
+
+print(paste0("INFO [", Sys.time(), "] Loading Species String Definitions"))
 source("./Scripts/species_string_definitions.R") # species strings
 
 # Set inputs for workflow
@@ -16,4 +22,4 @@ cropped <- apsn_cropped # cropped?
 # source/run workflow
 source("./Scripts/rest_run_workflow.R")
 
-print(paste0("APPLE SNAIL :: END -- ", Sys.time()))
+print(paste0("INFO [", Sys.time(), "] Apple Snail :: END"))
