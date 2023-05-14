1070d7d8b52a3d81ce3e6e199e05ed59527ebe96
Caitlin Hackett <chackett@usgs.gov>
2023-05-14 16:04:14 -0400

    add print/INFO statements

--- Scripts/run_kitenest.R
+++ Scripts/run_kitenest.R
@@ -1,10 +1,16 @@
 ## -----------------------------------------------------------------------------
 # Run KiteNest workflow
 ## -----------------------------------------------------------------------------
-print(paste0("KITENEST :: START -- ", Sys.time()))
+print(paste0("INFO [", Sys.time(), "] KiteNest :: START"))
+
+# Install any necessary packages that are not yet installed
+source("./Scripts/workflow_packages.R")
 
 # Source inputs/definitions
+print(paste0("INFO [", Sys.time(), "] Loading Workflow inputs"))
 source("./Scripts/workflow_inputs.R") # workflow inputs
+
+print(paste0("INFO [", Sys.time(), "] Loading Species String Definitions"))
 source("./Scripts/species_string_definitions.R") # species strings
 
 # Set inputs for workflow
@@ -16,4 +22,4 @@ cropped <- snki_cropped # cropped?
 # source/run workflow
 source("./Scripts/rest_run_workflow.R")
 
-print(paste0("KITENEST :: END -- ", Sys.time()))
+print(paste0("INFO [", Sys.time(), "] KiteNest :: END"))
