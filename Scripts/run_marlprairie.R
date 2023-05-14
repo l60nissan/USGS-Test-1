f5360340d21a7a8794de66b2596c4666e1f17801
Caitlin Hackett <chackett@usgs.gov>
2023-05-14 16:06:59 -0400

    add print/INFo statements

--- Scripts/run_marlprairie.R
+++ Scripts/run_marlprairie.R
@@ -1,9 +1,13 @@
 ## -----------------------------------------------------------------------------
 # Run Marl Prairie workflow
 ## -----------------------------------------------------------------------------
-print(paste0("Marl Prairie :: START -- ", Sys.time()))
+print(paste0("INFO [", Sys.time(), "] Marl Prairie :: START"))
+
+# Install any necessary packages that are not yet installed
+source("./Scripts/workflow_packages.R")
 
 # Source inputs/definitions
+print(paste0("INFO [", Sys.time(), "] Loading Workflow inputs"))
 source("./Scripts/workflow_inputs.R") # workflow inputs
 
 # Set inputs for workflow
@@ -13,4 +17,4 @@ output_path <- marl_output_path # output path
 # source/run workflow
 source("./Scripts/marlprairie_workflow.R")
 
-print(paste0("Marl Prairie :: END -- ", Sys.time()))
+print(paste0("INFO [", Sys.time(), "] Marl Prairie :: END"))
