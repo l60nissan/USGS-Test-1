16273bad6715103898b26e18ccd65da7a0937703
Caitlin Hackett <chackett@usgs.gov>
2023-05-15 10:56:38 -0400

    set input paths to work with GIS_Library folder in working directory

--- Scripts/input_paths.R
+++ Scripts/input_paths.R
@@ -4,17 +4,17 @@
 ## -----------------------------------------------------------------------------
 
 # Main Park Road
-mpr_path <- "../../GIS_Library/ENP_main_road/ENP_main_road.shp" 
+mpr_path <- "./GIS_Library/ENP_main_road/ENP_main_road.shp" 
 
 #WCAs Boundary
-wcas_path <- "../../GIS_Library/EVERareas_UTM/EVERareas_UTM/EVERareas_UTM.shp" 
+wcas_path <- "./GIS_Library/EVERareas_UTM/EVERareas_UTM/EVERareas_UTM.shp" 
 
 # florida outline
-fl_path <- "../../GIS_Library/FL_outline_ESRI_2010/FL_outline_ESRI_2010/FL_outline_ESRI_2010.shp" 
+fl_path <- "./GIS_Library/FL_outline_ESRI_2010/FL_outline_ESRI_2010/FL_outline_ESRI_2010.shp" 
 
 # Wood stork colonies
-wost_path <- "../../GIS_Library/WOST_colonies/WOST_colonies_JB.shp"
+wost_path <- "./GIS_Library/WOST_colonies/WOST_colonies_JB.shp"
 
 # CSSS subpopulation - includes AX
-subpop_path <- "../../GIS_Library/CSSS_subpopulations_wAX/CSSS_subpopulations_wAX.shp" 
+subpop_path <- "./GIS_Library/CSSS_subpopulations_wAX/CSSS_subpopulations_wAX.shp" 
 
