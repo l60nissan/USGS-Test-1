6bd3b4a6d43d4b1c5d04e33b9d6acf0e368f69f5
Caitlin Hackett <chackett@usgs.gov>
2023-06-28 14:23:34 -0400

    add descriptors to function arguments and apply lintr style suggestions

--- Scripts/smallfish_barplot_functions.R
+++ Scripts/smallfish_barplot_functions.R
@@ -1,21 +1,19 @@
 ## -----------------------------------------------------------------------------
-# Function to generate fish barplot for restoration runs
-#
-# Caitlin Hackett chackett@usgs.gov
+# Function to generate fish bar plot for restoration runs
 ## -----------------------------------------------------------------------------
 
 ## -----------------------------------------------------------------------------
 ## Function for percent diff Bar plot -> 1 alt and baselines
 ## -----------------------------------------------------------------------------
-PerDiffPlot <- function(df,
-                        x_var,
-                        y_var,
-                        fill_var,
-                        title,
-                        y_lab,
-                        x_lab,
-                        min_limit,
-                        max_limit){
+PerDiffPlot <- function(df, # data frame to plot
+                        x_var, # string of x variable
+                        y_var, # string of y variable
+                        fill_var, # string of fill variable
+                        title, # string of plot title
+                        y_lab, # string of y label
+                        x_lab, # string of x label
+                        min_limit, # value of minimum value for y scale
+                        max_limit) { # value of maximum value for y value
   diff_plot <- ggplot(data = df,
                       aes(x = !!sym(x_var), y = !!sym(y_var),
                           fill = !!sym(fill_var))) + 
@@ -42,22 +40,22 @@ PerDiffPlot <- function(df,
 ## Function for percent diff Bar plot -> alts with 1 baseline
 ## -----------------------------------------------------------------------------
 
-PerDiffPlotAlts <- function(df,
-                            x_var,
-                            y_var,
-                            fill_var,
-                            title,
-                            y_lab,
-                            x_lab,
-                            min_limit,
-                            max_limit){
+PerDiffPlotAlts <- function(df, # data frame to plot
+                            x_var, # string of x variable
+                            y_var, # string of y variable
+                            fill_var, # string of fill variable
+                            title, # string of plot title
+                            y_lab, # string of y label
+                            x_lab, # string of x label
+                            min_limit, # value of minimum value for y scale
+                            max_limit) { # value of maximum value for y scale
   bar_pal <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00")
   diff_plot <- ggplot(data = df,
                              aes(x = !!sym(x_var), y = !!sym(y_var),
                                  fill = !!sym(fill_var))) + 
     geom_bar(stat = "identity", position = "dodge",
              width = 0.7, colour = "black") + 
-    scale_fill_manual(values = bar_pal ) +
+    scale_fill_manual(values = bar_pal) +
     labs(y = y_lab, x = x_lab, title = title,
          fill = "percent change \nfrom baseline:") +
     scale_y_continuous(limits = c(min_limit, max_limit)) +
@@ -70,7 +68,7 @@ PerDiffPlotAlts <- function(df,
           legend.title = element_text(size = 15), 
           legend.title.align = 0.5,
           # Set margins for plot - wider on right side to make space fore legend
-          plot.margin = margin(1,.5,1,.5, unit = "in")) 
+          plot.margin = margin(1, .5, 1, .5, unit = "in")) 
   
   return(diff_plot)
 }
