f24baeda4aefbf3e287af14da5b643479f20e0a7
Caitlin Hackett <chackett@usgs.gov>
2023-06-30 15:48:07 -0400

    Add warning and output information to heading

--- Scripts/run_kitenest.R
+++ Scripts/run_kitenest.R
@@ -1,6 +1,22 @@
 ## -----------------------------------------------------------------------------
 # Run KiteNest workflow
+#
+# Summarize output of KiteNest output for use by restoration managers and planners
+# in restoration planning
+# outputs are: bar plots, maps, CSVs, and RData of processed output
 ## -----------------------------------------------------------------------------
+# -----
+# Runtime: Dependent on the total number of scenarios (Alternative + baseline)
+#          being processed. ~ 1 hour per scenario
+# ----- 
+# Expected warnings:
+# - When working with NetCDFs in R, some warnings will be triggered
+# regarding the CRS but these can be ignored. 
+#
+# - "Warning message: attribute variables are assumed to be spatially
+#   constant throughout all geometries"
+## -----------------------------------------------------------------------------
+
 print(paste0("INFO [", Sys.time(), "] KiteNest :: START"))
 
 # Install any necessary packages that are not yet installed
