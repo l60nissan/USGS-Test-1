79406056578871ab013b1f932046d82ac68d2718
Caitlin Hackett <chackett@usgs.gov>
2023-06-30 16:19:25 -0400

    add comments to heading and update fish_path argument

--- Scripts/run_smallfish.R
+++ Scripts/run_smallfish.R
@@ -1,6 +1,15 @@
 ## -----------------------------------------------------------------------------
 # Run Small Fish workflow
+
+# Run routine to summarize Small Fish Density Model output
+# Script outputs are: line graph, bar plots, maps, txt files,
+#                      and RData of processed output
 ## -----------------------------------------------------------------------------
+# Expected warnings:
+# - "Warning message: attribute variables are assumed to be spatially
+#   constant throughout all geometries"
+## -----------------------------------------------------------------------------
+
 print(paste0("INFO [", Sys.time(), "] Small Fish :: START"))
 
 # Install any necessary packages that are not yet installed
@@ -12,7 +21,7 @@ source("./Scripts/workflow_inputs.R") # workflow inputs
 
 # Set inputs for workflow
 parent_path <- fish_parent_path # parent path
-parent_path <- list.files(parent_path, pattern = "\\FISH_TIMESERIES_PSU.csv",
+fish_path <- list.files(parent_path, pattern = "\\FISH_TIMESERIES_PSU.csv",
                           full.names = TRUE, recursive = TRUE)
 output_path <- fish_output_path # output path
 
