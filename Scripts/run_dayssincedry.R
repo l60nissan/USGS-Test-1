ff6e1905e14646744de9c58a31bcf82a23cbefc9
Caitlin Hackett <chackett@usgs.gov>
2023-06-30 16:09:06 -0400

    add expected warnings to heading

--- Scripts/run_dayssincedry.R
+++ Scripts/run_dayssincedry.R
@@ -4,6 +4,14 @@
 # Run routine to summarize DaysSinceDry output
 # Script outputs are: bar plots, maps, CSVs, and RData of processed output
 ## -----------------------------------------------------------------------------
+# Expected warnings:
+# - When working with NetCDFs in R, some warnings will be triggered
+# regarding the CRS but these can be ignored. 
+#
+# - "Warning message: attribute variables are assumed to be spatially
+#   constant throughout all geometries"
+## -----------------------------------------------------------------------------
+
 print(paste0("INFO [", Sys.time(), "] Days Since Dry :: START"))
 
 # Install any necessary packages that are not yet installed
